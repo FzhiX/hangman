83b2fb5b099bee6a52decbbdde021b0e1c539274
FzhiX <fanzhi1011@gmail.com>
2024-02-23 14:04:28 +0100

    Updated

--- src/App.java
+++ src/App.java
@@ -1,32 +1,32 @@
+import classes.*;
 import processing.core.*;
 
-import classes.*;
+public class App extends PApplet {
 
-public class App  extends PApplet{
+  Hangman game;
 
-    Hangman game;
-    public static void main(String[] args) {
-        String[] processingArgs = {"MySketch"};
-		App mySketch = new App();
-		PApplet.runSketch(processingArgs, mySketch);
+  public static void main(String[] args) {
+    String[] processingArgs = { "MySketch" };
+    App mySketch = new App();
+    PApplet.runSketch(processingArgs, mySketch);
+  }
+
+  public void settings() {
+    size(600, 400);
+  }
+
+  public void setup() {
+    game = new Hangman("Multiplication");
+  }
+
+  public void draw() {
+    background(0);
+    game.drawGuessedLetters(this);
+  }
+
+  public void keyPressed() {
+    if (key != CODED) {
+      game.guess(key);
     }
-
-    public void settings(){
-        size(600, 400);
-    }
-
-    public void setup(){
-        game = new Hangman("Multiplication");
-    }
-
-    public void draw(){
-        background(0);
-        game.drawGuessedLetters(this);
-    }
-
-    public void keyPressed(){
-        if (key != CODED){
-            game.guess(key);
-        }
-    }
+  }
 }
--- src/classes/Hangman.java
+++ src/classes/Hangman.java
@@ -1,51 +1,96 @@
 package classes;
 
 import processing.core.PApplet;
+import processing.core.PConstants;
 
 public class Hangman {
-    private String secretWord;
-    private char[] guessedLetters;
 
-    public Hangman(String word){
-        secretWord = word.toLowerCase();
-        guessedLetters = new char[secretWord.length()];
+  private String secretWord, drawWord;
+  private char[] guessedLetters;
+  private int lives;
+  private int[] lettersIndex;
 
-        //init partial word, and set all
-        //letters to '_'
-        guessedLetters = new char[word.length()];
-        for (int i = 0; i < guessedLetters.length; i++){
-            guessedLetters[i] = '_';
-        }
+  public Hangman(String word) {
+    secretWord = word;
+    drawWord = word;
+    secretWord = secretWord.toLowerCase();
+    guessedLetters = new char[secretWord.length()];
+
+    lives = 6;
+
+    lettersIndex = new int[255];
+    for (int i = 0; i < 255; i++) {
+      lettersIndex[i] = 0;
     }
 
-    public char[] getGuessedLetters(){
-        return guessedLetters;
+    //init partial word, and set all
+    //letters to '_'
+    guessedLetters = new char[word.length()];
+    for (int i = 0; i < guessedLetters.length; i++) {
+      guessedLetters[i] = '_';
+    }
+  }
+
+  public char[] getGuessedLetters() {
+    return guessedLetters;
+  }
+
+  public boolean guess(char letter) {
+    if (lives <= 0) {
+      return false;
     }
 
-    public boolean guess(char letter){
-        char lowerCase = Character.toLowerCase(letter);
-        
-        boolean letterInSecretWord = false;
-        for (int i = 0; i < secretWord.length(); i++){
-            if (secretWord.charAt(i) == lowerCase){
-              guessedLetters[i] = lowerCase;
-              letterInSecretWord = true;
-            }
-          }
-        return letterInSecretWord;
+    boolean found = false;
+    letter = Character.toLowerCase(letter);
+
+    for (int i = 0; i < secretWord.length(); i++) {
+      if (secretWord.charAt(i) == letter) {
+        guessedLetters[i] = letter;
+        found = true;
+      }
     }
 
-    public void drawGuessedLetters(PApplet p){
-        //draw word on screen
-        p.textSize(32);
-        for (int i = 0; i < guessedLetters.length; i++){
-            //draw guessed letter in center of the screen
-            p.text(guessedLetters[i], p.width/2+20*i-
-                guessedLetters.length*20/2, p.height/2);
-        }
+    if (found == false) {
+      if (lettersIndex[letter] == 1) {
+        return false;
+      }
+      lives--;
+      lettersIndex[letter] = 1;
     }
 
-    public boolean isAlive(){
-        return true;
+    return found;
+  }
+
+  public void drawGuessedLetters(PApplet p) {
+    //draw word on screen
+    p.textSize(32);
+    for (int i = 0; i < guessedLetters.length; i++) {
+      //draw guessed letter in center of the screen
+      p.text(
+        guessedLetters[i],
+        p.width / 2 + 20 * i - guessedLetters.length * 20 / 2,
+        p.height / 2
+      );
     }
+    drawGameState(p);
+  }
+
+  public boolean isAlive() {
+    if (lives <= 0) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  private void drawGameState(PApplet p) {
+    p.textSize(32);
+    p.textAlign(PConstants.CENTER, PConstants.CENTER);
+
+    if (isAlive() == true) {
+      p.text("Lives: " + lives, p.width / 2, p.height / 2);
+    } else if (isAlive() == false) {
+      p.text("Game Over", p.width / 2, p.height / 2 + 40);
+    }
+  }
 }
